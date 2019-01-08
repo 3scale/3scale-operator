ac80fa1ebabe2cdc69510c4078a0188cb05239ab
Miguel Soriano <msoriano@redhat.com>
2019-01-08 20:35:27 +0100

    Add Operator Option Providers for 3scale elements

---
+++ pkg/3scale/amp/operator/redis.go
@@ -0,0 +1,18 @@
+package operator
+
+import (
+	"fmt"
+
+	"github.com/3scale/3scale-operator/pkg/3scale/amp/component"
+)
+
+func (o *OperatorRedisOptionsProvider) GetRedisOptions() (*component.RedisOptions, error) {
+	optProv := component.RedisOptionsBuilder{}
+	optProv.AppLabel(*o.AmpSpec.AppLabel)
+	optProv.Image(*o.AmpSpec.RedisImage)
+	res, err := optProv.Build()
+	if err != nil {
+		return nil, fmt.Errorf("unable to create Redis Options - %s", err)
+	}
+	return res, nil
+}
