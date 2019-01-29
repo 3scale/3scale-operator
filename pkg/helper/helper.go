fcb8598c07ec46030913c27b87596fa323ef90a1
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-01-29 17:17:54 +0100

    controller/helper: helper methods

---
+++ pkg/helper/helper.go
@@ -0,0 +1,49 @@
+package helper
+
+import (
+	"crypto/tls"
+	"net/http"
+	"net/url"
+	"strconv"
+
+	"github.com/3scale/3scale-porta-go-client/client"
+)
+
+// PortaClientFromURLString instantiate porta_client.ThreeScaleClient from admin url string
+func PortaClientFromURLString(adminURLStr string) (*client.ThreeScaleClient, error) {
+	adminURL, err := url.Parse(adminURLStr)
+	if err != nil {
+		return nil, err
+	}
+	return PortaClient(adminURL)
+}
+
+// PortaClient instantiates porta_client.ThreeScaleClient from admin url object
+func PortaClient(url *url.URL) (*client.ThreeScaleClient, error) {
+	adminPortal, err := client.NewAdminPortal(url.Scheme, url.Hostname(), PortFromURL(url))
+	if err != nil {
+		return nil, err
+	}
+
+	tr := &http.Transport{
+		TLSClientConfig: &tls.Config{InsecureSkipVerify: true},
+	}
+	return client.NewThreeScale(adminPortal, &http.Client{Transport: tr}), nil
+}
+
+// PortFromURL infers port number if it is not explict
+func PortFromURL(url *url.URL) int {
+	if url.Port() != "" {
+		if portNum, err := strconv.Atoi(url.Port()); err == nil {
+			return portNum
+		}
+	}
+
+	// Default HTTP port numbers
+	portNum := 80
+	// Scheme is always lowercase
+	if url.Scheme == "https" {
+		portNum = 443
+	}
+	return portNum
+}
