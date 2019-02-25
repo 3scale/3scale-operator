41b80715a637070845de5cb4f714476875ddb1b5
Joaquim Moreno Prusi <jmprusi@keepalive.io>
2019-02-25 14:03:34 +0100

    Upgrade operator-sdk to latest version.
    - We are using the latest revision in operator-sdk because 0.5.0 has a
    bug when running in local mode (--up-local)
    
    - Updated Dockerfile to match the latest operator-sdk version, also,
    added two new files, entrypoint and user_setup.
    
    - Regenerated the deepcopy functions using the latest version
    
    - Updated the cmd/manager/main.go to include the latest generated
    version.
    
    - Updated operator.yaml file to match the latest generated version

--- cmd/manager/main.go
+++ cmd/manager/main.go
@@ -4,6 +4,9 @@ import (
 	"context"
 	"flag"
 	"fmt"
+	"github.com/operator-framework/operator-sdk/pkg/log/zap"
+	"github.com/operator-framework/operator-sdk/pkg/metrics"
+	"github.com/spf13/pflag"
 	"os"
 	"runtime"
 
@@ -11,13 +14,17 @@ import (
 	"github.com/3scale/3scale-operator/pkg/controller"
 	"github.com/operator-framework/operator-sdk/pkg/k8sutil"
 	"github.com/operator-framework/operator-sdk/pkg/leader"
-	"github.com/operator-framework/operator-sdk/pkg/ready"
 	sdkVersion "github.com/operator-framework/operator-sdk/version"
 	_ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
 	"sigs.k8s.io/controller-runtime/pkg/client/config"
 	"sigs.k8s.io/controller-runtime/pkg/manager"
 	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
 	"sigs.k8s.io/controller-runtime/pkg/runtime/signals"
+)
+
+var (
+	metricsHost       = "0.0.0.0"
+	metricsPort int32 = 8383
 )
 
 var log = logf.Log.WithName("cmd")
@@ -29,8 +36,13 @@ func printVersion() {
 }
 
 func main() {
-	flag.Parse()
+	pflag.CommandLine.AddFlagSet(zap.FlagSet())
 
+	// Add flags registered by imported packages (e.g. glog and
+	// controller-runtime)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+
+	pflag.Parse()
 	// The logger instantiated here can be changed to any logger
 	// implementing the logr.Logger interface. This logger will
 	// be propagated through the whole operator, generating
@@ -41,7 +53,7 @@ func main() {
 
 	namespace, err := k8sutil.GetWatchNamespace()
 	if err != nil {
-		log.Error(err, "failed to get watch namespace")
+		log.Error(err, "Failed to get watch namespace")
 		os.Exit(1)
 	}
 
@@ -52,19 +64,20 @@ func main() {
 		os.Exit(1)
 	}
 
-	// Become the leader before proceeding
-	leader.Become(context.TODO(), "3scale-operator-lock")
+	ctx := context.TODO()
 
-	r := ready.NewFileReady()
-	err = r.Set()
+	// Become the leader before proceeding
+	err = leader.Become(ctx, "3scale-operator-lock")
 	if err != nil {
 		log.Error(err, "")
 		os.Exit(1)
 	}
-	defer r.Unset()
 
 	// Create a new Cmd to provide shared dependencies and start components
-	mgr, err := manager.New(cfg, manager.Options{Namespace: namespace})
+	mgr, err := manager.New(cfg, manager.Options{
+		Namespace:          namespace,
+		MetricsBindAddress: fmt.Sprintf("%s:%d", metricsHost, metricsPort),
+	})
 	if err != nil {
 		log.Error(err, "")
 		os.Exit(1)
@@ -84,11 +97,18 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Create Service object to expose the metrics port.
+	_, err = metrics.ExposeMetricsPort(ctx, metricsPort)
+	if err != nil {
+		log.Info(err.Error())
+	}
+
 	log.Info("Starting the Cmd.")
 
 	// Start the Cmd
 	if err := mgr.Start(signals.SetupSignalHandler()); err != nil {
-		log.Error(err, "manager exited non-zero")
+		log.Error(err, "Manager exited non-zero")
 		os.Exit(1)
 	}
+
 }
