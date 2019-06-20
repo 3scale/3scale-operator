14f7dfef9edc33c2553732a27fd5867a3c361aed
Miguel Soriano <msoriano@redhat.com>
2019-06-20 12:24:05 +0200

    Update images for 2.6 release in operator and templates

--- pkg/3scale/amp/product/images.go
+++ pkg/3scale/amp/product/images.go
@@ -7,12 +7,15 @@ type Version string
 const (
 	ProductUpstream    Version = "upstream"
 	ProductRelease_2_5 Version = "2.5"
+	ProductRelease_2_6 Version = "2.6"
 )
 
 func NewImageProvider(productVersion Version) (ImageProvider, error) {
 	switch productVersion {
 	case ProductRelease_2_5:
 		return &release_2_5{}, nil
+	case ProductRelease_2_6:
+		return &release_2_6{}, nil
 	case ProductUpstream:
 		return &upstream{}, nil
 	default:
@@ -38,5 +41,5 @@ func IsProductizedVersion(productVersion Version) bool {
 }
 
 func CurrentProductVersion() Version {
-	return ProductRelease_2_5
+	return ProductRelease_2_6
 }
