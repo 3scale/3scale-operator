35a486168919c06f01ebc9a66e4ed21265d12d83
Miguel Soriano <msoriano@redhat.com>
2019-07-02 12:10:13 +0200

    Unify image URLs source between templates and operator

--- pkg/3scale/amp/template/adapters/redis.go
+++ pkg/3scale/amp/template/adapters/redis.go
@@ -2,6 +2,7 @@ package adapters
 
 import (
 	"github.com/3scale/3scale-operator/pkg/3scale/amp/component"
+	"github.com/3scale/3scale-operator/pkg/3scale/amp/product"
 	"github.com/3scale/3scale-operator/pkg/common"
 	templatev1 "github.com/openshift/api/template/v1"
 )
@@ -14,12 +15,20 @@ func NewRedisAdapter(options []string) Adapter {
 }
 
 func (a *RedisAdapter) Parameters() []templatev1.Parameter {
+	productVersion := product.CurrentProductVersion()
+	imageProvider, err := product.NewImageProvider(productVersion)
+	if err != nil {
+		panic(err)
+	}
 	return []templatev1.Parameter{
 		{
 			Name:        "REDIS_IMAGE",
 			Description: "Redis image to use",
 			Required:    true,
-			Value:       "centos/redis-32-centos7",
+			// We use backend-redis image because we have to choose one
+			// but in templates there's no distinction between Backend Redis image
+			// used and System Redis image. They are always the same
+			Value: imageProvider.GetBackendRedisImage(),
 		},
 	}
 }
