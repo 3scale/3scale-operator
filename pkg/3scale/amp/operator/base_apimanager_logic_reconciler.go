30bed27f460f8d1fb235e78d734d3aa6bbb56321
Miguel Soriano <msoriano@redhat.com>
2019-07-08 18:34:30 +0200

    Add method in BaseAPIManagerLogicReconciler to get NamespacedName of an object in APIManager namespace

--- pkg/3scale/amp/operator/base_apimanager_logic_reconciler.go
+++ pkg/3scale/amp/operator/base_apimanager_logic_reconciler.go
@@ -2,6 +2,8 @@ package operator
 
 import (
 	appsv1alpha1 "github.com/3scale/3scale-operator/pkg/apis/apps/v1alpha1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/types"
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
@@ -22,4 +24,8 @@ func NewBaseAPIManagerLogicReconciler(b BaseLogicReconciler, apiManager *appsv1a
 
 func (r BaseAPIManagerLogicReconciler) Reconcile() (reconcile.Result, error) {
 	return reconcile.Result{}, nil
+}
+
+func (r BaseAPIManagerLogicReconciler) NamespacedNameWithAPIManagerNamespace(obj metav1.Object) types.NamespacedName {
+	return types.NamespacedName{Namespace: r.apiManager.GetNamespace(), Name: obj.GetName()}
 }
