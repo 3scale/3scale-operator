cad11d6cf734464cc3741d66bbdef23779dea057
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-08-26 13:55:29 +0200

    secret reconciler

--- pkg/3scale/amp/operator/secret_reconciler.go
+++ pkg/3scale/amp/operator/secret_reconciler.go
@@ -3,88 +3,128 @@ package operator
 import (
 	"context"
 	"fmt"
-	"reflect"
 
+	"github.com/3scale/3scale-operator/pkg/helper"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/errors"
 	"k8s.io/apimachinery/pkg/types"
 )
 
-type SecretReconciler struct {
-	BaseReconciler
-	ObjectMetaMerger ObjectMetaMerger
+type SecretReconciler interface {
+	IsUpdateNeeded(desired, existing *v1.Secret) bool
 }
 
-func NewSecretReconciler(baseReconciler BaseReconciler, objectMetaMerger ObjectMetaMerger) SecretReconciler {
-	return SecretReconciler{
-		BaseReconciler:   baseReconciler,
-		ObjectMetaMerger: objectMetaMerger,
+type SecretBaseReconciler struct {
+	BaseAPIManagerLogicReconciler
+	reconciler SecretReconciler
+}
+
+func NewSecretBaseReconciler(baseAPIManagerLogicReconciler BaseAPIManagerLogicReconciler, reconciler SecretReconciler) *SecretBaseReconciler {
+	return &SecretBaseReconciler{
+		BaseAPIManagerLogicReconciler: baseAPIManagerLogicReconciler,
+		reconciler:                    reconciler,
 	}
 }
 
-// TODO should this be a shared Secret reconcile behaviour or could there be
-// different secrets where we reconcile them in a different way?
-// For example, secrets where we just want to check if they are installed it
-// but we don't want to reconcile them. That would make sense for example
-// for system-seed secret.
-// Also in the case where we wanted to reconcile them but we wanted to do it
-// in a different way
-func (r *SecretReconciler) Reconcile(desiredSecret *v1.Secret) error {
-	objectInfo := ObjectInfo(desiredSecret)
-	existingSecret := &v1.Secret{}
-	err := r.Client().Get(context.TODO(), types.NamespacedName{Name: desiredSecret.Name, Namespace: desiredSecret.Namespace}, existingSecret)
+func (r *SecretBaseReconciler) Reconcile(desired *v1.Secret) error {
+	objectInfo := ObjectInfo(desired)
+	existing := &v1.Secret{}
+	err := r.Client().Get(
+		context.TODO(),
+		types.NamespacedName{Name: desired.Name, Namespace: r.apiManager.GetNamespace()},
+		existing)
 	if err != nil {
 		if errors.IsNotFound(err) {
-			createErr := r.Client().Create(context.TODO(), desiredSecret)
+			createErr := r.createResource(desired)
 			if createErr != nil {
 				r.Logger().Error(createErr, fmt.Sprintf("Error creating object %s. Requeuing request...", objectInfo))
 				return createErr
 			}
-			r.Logger().Info(fmt.Sprintf("Created object %s", objectInfo))
 			return nil
 		}
 		return err
 	}
 
-	needsUpdate, err := r.ensureSecret(existingSecret, desiredSecret)
+	update, err := r.isUpdateNeeded(desired, existing)
+	if err != nil {
+		return err
+	}
 
-	if needsUpdate {
-		r.Logger().Info(fmt.Sprintf("Updating Secret %s", objectInfo))
-		err := r.Client().Update(context.TODO(), existingSecret)
-		if err != nil {
-			r.Logger().Error(err, fmt.Sprintf("error updating Service %s", objectInfo))
-			return err
-		}
+	if update {
+		return r.updateResource(existing)
 	}
 
 	return nil
 }
 
-func (r *SecretReconciler) ensureSecret(updated, desired *v1.Secret) (bool, error) {
-	changed := false
+func (r *SecretBaseReconciler) isUpdateNeeded(desired, existing *v1.Secret) (bool, error) {
+	updated := helper.EnsureObjectMeta(&existing.ObjectMeta, &desired.ObjectMeta)
 
-	objectMetaChanged, err := r.ObjectMetaMerger.EnsureObjectMeta(&updated.ObjectMeta, &desired.ObjectMeta)
+	updatedTmp, err := r.ensureOwnerReference(existing)
 	if err != nil {
-		return false, err
-	}
-	if objectMetaChanged {
-		changed = true
+		return false, nil
 	}
 
-	// TODO writing on StringData has merge behaviour with existing Data content.
-	// Do we want this or do we want to always overwrite everything? Take note that
-	// in ConfigMap for example it seems there's no merge behavior by itself so
-	// either we implement it by ourselves or we would have different behavior
-	// between secret and configmap fields reconciliation
-	// TODO there's also a case where a StringData field that is set to empty
-	// is encoded as the empty string in the Data section but when there's another
-	// update it is encoded to null, and that would be detected as a difference.
-	// Would that be dangerous/a problem?
-	updatedSecretStringData := getSecretStringDataFromData(updated.Data)
-	if !reflect.DeepEqual(updatedSecretStringData, desired.StringData) {
-		updated.StringData = desired.StringData
-		changed = true
+	updated = updated || updatedTmp
+
+	updatedTmp = r.reconciler.IsUpdateNeeded(desired, existing)
+	updated = updated || updatedTmp
+
+	return updated, nil
+}
+
+type CreateOnlySecretReconciler struct {
+}
+
+func NewCreateOnlySecretReconciler() *CreateOnlySecretReconciler {
+	return &CreateOnlySecretReconciler{}
+}
+
+func (r *CreateOnlySecretReconciler) IsUpdateNeeded(desired, existing *v1.Secret) bool {
+	return false
+}
+
+type DefaultsOnlySecretReconciler struct {
+}
+
+// Reconciler for defaults only is useful for secrets pre-created by the user and when not all the fields are created.
+// Fields referenced from deployment configs must exist,
+// so defaults only reconciliation makes sure they exist with default values when user does doe pre-create them
+func NewDefaultsOnlySecretReconciler() *DefaultsOnlySecretReconciler {
+	return &DefaultsOnlySecretReconciler{}
+}
+
+func (r *DefaultsOnlySecretReconciler) IsUpdateNeeded(desired, existing *v1.Secret) bool {
+	updated := false
+
+	if existing.StringData == nil {
+		existing.StringData = map[string]string{}
 	}
 
-	return changed, nil
+	for k, v := range desired.StringData {
+		if _, ok := existing.Data[k]; !ok {
+			existing.StringData[k] = v
+			updated = true
+		}
+	}
+
+	return updated
+}
+
+func SecretReconcileField(desired, existing *v1.Secret, fieldName string) bool {
+	updated := false
+
+	valB, ok := existing.Data[fieldName]
+	if !ok {
+		existing.StringData[fieldName] = desired.StringData[fieldName]
+		updated = true
+	} else {
+		valStr := string(valB)
+		if desired.StringData[fieldName] != valStr {
+			// should merge existing key in Data struct
+			existing.StringData[fieldName] = desired.StringData[fieldName]
+			updated = true
+		}
+	}
+	return updated
 }
