289a7f1ddc0579682d511623d5936f727ae62473
Miguel Soriano <msoriano@redhat.com>
2019-09-27 12:12:56 +0200

    Add routes checking in e2e tests

--- test/e2e/e2eutil/wait.go
+++ test/e2e/e2eutil/wait.go
@@ -2,17 +2,21 @@ package e2eutil
 
 import (
 	"context"
+	"fmt"
+	"testing"
+	"time"
+
 	"github.com/3scale/3scale-operator/pkg/apis/capabilities/v1alpha1"
 	"github.com/operator-framework/operator-sdk/pkg/test"
 	"k8s.io/apimachinery/pkg/types"
-	"testing"
-	"time"
 
 	"k8s.io/apimachinery/pkg/util/wait"
 	"k8s.io/client-go/kubernetes"
 
 	appsv1 "github.com/openshift/api/apps/v1"
+	routev1 "github.com/openshift/api/route/v1"
 	clientappsv1 "github.com/openshift/client-go/apps/clientset/versioned/typed/apps/v1"
+	clientroutev1 "github.com/openshift/client-go/route/clientset/versioned/typed/route/v1"
 	corev1 "k8s.io/api/core/v1"
 
 	apierrors "k8s.io/apimachinery/pkg/api/errors"
@@ -93,4 +97,61 @@ func WaitForReconciliationWith3scale(t *testing.T, c test.FrameworkClient, bindi
 	}
 	return nil
 
+}
+
+func WaitForRouteFromHost(t *testing.T, kubeClient kubernetes.Interface, osRouteV1Client clientroutev1.RouteV1Interface, namespace, host string, retryInterval, timeout time.Duration) error {
+	err := wait.Poll(retryInterval, timeout, func() (done bool, err error) {
+		routeInteface := osRouteV1Client.Routes(namespace)
+		routeFieldSelector := "spec.host=" + host
+		routeList, err := routeInteface.List(
+			metav1.ListOptions{
+				IncludeUninitialized: true,
+				FieldSelector:        routeFieldSelector},
+		)
+		if err != nil {
+			if apierrors.IsNotFound(err) {
+				t.Logf("Waiting for availability of Route with host '%s'\n", host)
+				return false, nil
+			}
+			return false, err
+		}
+
+		routeItems := routeList.Items
+		if len(routeItems) == 0 {
+			t.Logf("Waiting for availability of Route with host '%s'\n", host)
+			return false, nil
+		}
+		if len(routeItems) > 1 {
+			return false, fmt.Errorf("Found unexpected routes with duplicated 'host' fields")
+		}
+
+		route := routeItems[0]
+		routeStatusIngresses := route.Status.Ingress
+		if routeStatusIngresses == nil || len(routeStatusIngresses) == 0 {
+			t.Logf("Waiting for availability of Route with host '%s'\n", host)
+			return false, nil
+		}
+
+		for _, routeStatusIngress := range routeStatusIngresses {
+			routeStatusIngressConditions := routeStatusIngress.Conditions
+			isReady := false
+			for _, routeStatusIngressCondition := range routeStatusIngressConditions {
+				if routeStatusIngressCondition.Type == routev1.RouteAdmitted && routeStatusIngressCondition.Status == corev1.ConditionTrue {
+					isReady = true
+					break
+				}
+			}
+			if !isReady {
+				t.Logf("Waiting for availability of Route with host '%s'\n", host)
+				return false, nil
+			}
+		}
+
+		t.Logf("Route '%s' with host '%s' available\n", route.Name, route.Spec.Host)
+		return true, nil
+	})
+	if err != nil {
+		return err
+	}
+	return nil
 }
