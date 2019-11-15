9293457ddf8cac511c1dd72945846624bdeb6a44
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-11-15 16:01:25 +0100

    operator/system_postgresql unittests

---
+++ pkg/3scale/amp/operator/system_postgresql_image_reconciler_test.go
@@ -0,0 +1,79 @@
+package operator
+
+import (
+	"context"
+	"testing"
+
+	appsv1alpha1 "github.com/3scale/3scale-operator/pkg/apis/apps/v1alpha1"
+	imagev1 "github.com/openshift/api/image/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
+
+func TestSystemPostgreSQLImageReconcilerCreate(t *testing.T) {
+	var (
+		appLabel  = "someLabel"
+		name      = "example-apimanager"
+		namespace = "operator-unittest"
+		trueValue = true
+		imageUrl  = "postgresql:test"
+		log       = logf.Log.WithName("operator_test")
+	)
+	apimanager := &appsv1alpha1.APIManager{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      name,
+			Namespace: namespace,
+		},
+		Spec: appsv1alpha1.APIManagerSpec{
+			APIManagerCommonSpec: appsv1alpha1.APIManagerCommonSpec{
+				AppLabel:                     &appLabel,
+				ImageStreamTagImportInsecure: &trueValue,
+			},
+			System: &appsv1alpha1.SystemSpec{
+				DatabaseSpec: &appsv1alpha1.SystemDatabaseSpec{
+					PostgreSQL: &appsv1alpha1.SystemPostgreSQLSpec{
+						Image: &imageUrl,
+					},
+				},
+			},
+		},
+	}
+	s := scheme.Scheme
+	s.AddKnownTypes(appsv1alpha1.SchemeGroupVersion, apimanager)
+	err := imagev1.AddToScheme(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Objects to track in the fake client.
+	objs := []runtime.Object{}
+
+	// Create a fake client to mock API calls.
+	cl := fake.NewFakeClient(objs...)
+	clientAPIReader := fake.NewFakeClient(objs...)
+
+	baseReconciler := NewBaseReconciler(cl, clientAPIReader, s, log)
+	baseLogicReconciler := NewBaseLogicReconciler(baseReconciler)
+	baseAPIManagerLogicReconciler := NewBaseAPIManagerLogicReconciler(baseLogicReconciler, apimanager)
+	reconciler := NewSystemPostgreSQLImageReconciler(baseAPIManagerLogicReconciler)
+	_, err = reconciler.Reconcile()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	obj := &imagev1.ImageStream{}
+
+	namespacedName := types.NamespacedName{
+		Name:      "system-postgresql",
+		Namespace: namespace,
+	}
+	err = cl.Get(context.TODO(), namespacedName, obj)
+	// object must exist, that is all required to be tested
+	if err != nil {
+		t.Fatal(err)
+	}
+}
