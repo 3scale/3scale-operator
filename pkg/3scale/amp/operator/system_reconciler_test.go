a4b3a27ff968f896ce9b48a8edf3ffde86e07bf4
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-11-19 17:44:33 +0100

    operator/system_reconciler unittests

---
+++ pkg/3scale/amp/operator/system_reconciler_test.go
@@ -0,0 +1,99 @@
+package operator
+
+import (
+	"context"
+	"testing"
+
+	"github.com/3scale/3scale-operator/pkg/3scale/amp/component"
+	appsv1alpha1 "github.com/3scale/3scale-operator/pkg/apis/apps/v1alpha1"
+	appsv1 "github.com/openshift/api/apps/v1"
+	imagev1 "github.com/openshift/api/image/v1"
+	routev1 "github.com/openshift/api/route/v1"
+	v1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
+
+func TestSystemReconcilerCreate(t *testing.T) {
+	var (
+		name      = "example-apimanager"
+		namespace = "operator-unittest"
+		log       = logf.Log.WithName("operator_test")
+	)
+	apimanager := basicApimanagerSpecTestSystemOptions(name, namespace)
+	// Objects to track in the fake client.
+	objs := []runtime.Object{apimanager}
+	s := scheme.Scheme
+	s.AddKnownTypes(appsv1alpha1.SchemeGroupVersion, apimanager)
+	err := appsv1.AddToScheme(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = imagev1.AddToScheme(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+	err = routev1.AddToScheme(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Create a fake client to mock API calls.
+	cl := fake.NewFakeClient(objs...)
+	clientAPIReader := fake.NewFakeClient(objs...)
+
+	baseReconciler := NewBaseReconciler(cl, clientAPIReader, s, log)
+	baseLogicReconciler := NewBaseLogicReconciler(baseReconciler)
+	BaseAPIManagerLogicReconciler := NewBaseAPIManagerLogicReconciler(baseLogicReconciler, apimanager)
+
+	reconciler := NewSystemReconciler(BaseAPIManagerLogicReconciler)
+	_, err = reconciler.Reconcile()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	cases := []struct {
+		testName string
+		objName  string
+		obj      runtime.Object
+	}{
+		{"systemPVC", "system-storage", &v1.PersistentVolumeClaim{}},
+		{"systemProviderService", "system-provider", &v1.Service{}},
+		{"systemMasterService", "system-master", &v1.Service{}},
+		{"systemDeveloperService", "system-developer", &v1.Service{}},
+		{"systemSphinxService", "system-sphinx", &v1.Service{}},
+		{"systemMemcacheService", "system-memcache", &v1.Service{}},
+		{"systemAppDC", "system-app", &appsv1.DeploymentConfig{}},
+		{"systemSideKiqDC", "system-sidekiq", &appsv1.DeploymentConfig{}},
+		{"systemSphinxqDC", "system-sphinx", &appsv1.DeploymentConfig{}},
+		{"systemCM", "system", &v1.ConfigMap{}},
+		{"systemEnvironmentCM", "system-environment", &v1.ConfigMap{}},
+		{"systemSMTPCM", "smtp", &v1.ConfigMap{}},
+		{"systemEventsHookSecret", component.SystemSecretSystemEventsHookSecretName, &v1.Secret{}},
+		{"systemRedisSecret", component.SystemSecretSystemRedisSecretName, &v1.Secret{}},
+		{"systemMasterApicastSecret", component.SystemSecretSystemMasterApicastSecretName, &v1.Secret{}},
+		{"systemSeedSecret", component.SystemSecretSystemSeedSecretName, &v1.Secret{}},
+		{"systemRecaptchaSecret", component.SystemSecretSystemRecaptchaSecretName, &v1.Secret{}},
+		{"systemAppSecret", component.SystemSecretSystemAppSecretName, &v1.Secret{}},
+		{"systemMemcachedSecret", component.SystemSecretSystemMemcachedSecretName, &v1.Secret{}},
+		{"systemMemcachedSecret", component.SystemSecretSystemMemcachedSecretName, &v1.Secret{}},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.testName, func(subT *testing.T) {
+			obj := tc.obj
+			namespacedName := types.NamespacedName{
+				Name:      tc.objName,
+				Namespace: namespace,
+			}
+			err = cl.Get(context.TODO(), namespacedName, obj)
+			// object must exist, that is all required to be tested
+			if err != nil {
+				subT.Errorf("error fetching object %s: %v", tc.objName, err)
+			}
+		})
+	}
+}
