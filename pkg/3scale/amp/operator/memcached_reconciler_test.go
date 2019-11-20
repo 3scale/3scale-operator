eee3e709413c53a11acc2db1550f1fef2244bcfc
Eguzki Astiz Lezaun <eastizle@redhat.com>
2019-11-20 11:25:52 +0100

    operator/memcached_reconciler unittests

---
+++ pkg/3scale/amp/operator/memcached_reconciler_test.go
@@ -0,0 +1,67 @@
+package operator
+
+import (
+	"context"
+	"testing"
+
+	appsv1alpha1 "github.com/3scale/3scale-operator/pkg/apis/apps/v1alpha1"
+	appsv1 "github.com/openshift/api/apps/v1"
+	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
+	"k8s.io/client-go/kubernetes/scheme"
+	"sigs.k8s.io/controller-runtime/pkg/client/fake"
+	logf "sigs.k8s.io/controller-runtime/pkg/runtime/log"
+)
+
+func TestMemcachedDCReconciler(t *testing.T) {
+	var (
+		name      = "example-apimanager"
+		namespace = "operator-unittest"
+		log       = logf.Log.WithName("operator_test")
+	)
+	apimanager := basicApimanagerSpecTestOptions(name, namespace)
+	s := scheme.Scheme
+	s.AddKnownTypes(appsv1alpha1.SchemeGroupVersion, apimanager)
+	err := appsv1.AddToScheme(s)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// Objects to track in the fake client.
+	objs := []runtime.Object{}
+
+	// Create a fake client to mock API calls.
+	cl := fake.NewFakeClient(objs...)
+	clientAPIReader := fake.NewFakeClient(objs...)
+
+	baseReconciler := NewBaseReconciler(cl, clientAPIReader, s, log)
+	baseLogicReconciler := NewBaseLogicReconciler(baseReconciler)
+	baseAPIManagerLogicReconciler := NewBaseAPIManagerLogicReconciler(baseLogicReconciler, apimanager)
+	reconciler := NewMemcachedReconciler(baseAPIManagerLogicReconciler)
+	_, err = reconciler.Reconcile()
+	if err != nil {
+		t.Fatal(err)
+	}
+	cases := []struct {
+		testName string
+		objName  string
+		obj      runtime.Object
+	}{
+		{"memcachedDC", "system-memcache", &appsv1.DeploymentConfig{}},
+	}
+
+	for _, tc := range cases {
+		t.Run(tc.testName, func(subT *testing.T) {
+			obj := tc.obj
+			namespacedName := types.NamespacedName{
+				Name:      tc.objName,
+				Namespace: namespace,
+			}
+			err = cl.Get(context.TODO(), namespacedName, obj)
+			// object must exist, that is all required to be tested
+			if err != nil {
+				subT.Errorf("error fetching object %s: %v", tc.objName, err)
+			}
+		})
+	}
+}
