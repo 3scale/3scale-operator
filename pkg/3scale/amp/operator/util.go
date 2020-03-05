7b14e2d49b0401f8851dd6818050e137f801ccf1
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-03-05 17:22:30 +0100

    monitoring: utils to mark objects for deletion

--- pkg/3scale/amp/operator/util.go
+++ pkg/3scale/amp/operator/util.go
@@ -6,6 +6,30 @@ import (
 	"github.com/3scale/3scale-operator/pkg/common"
 )
 
+const (
+	DeleteTagAnnotation = "apps.3scale.net/delete"
+)
+
 func ObjectInfo(obj common.KubernetesObject) string {
 	return fmt.Sprintf("%s/%s", obj.GetObjectKind().GroupVersionKind().Kind, obj.GetName())
+}
+
+func TagObjectToDelete(obj common.KubernetesObject) {
+	// Add custom annotation
+	annotations := obj.GetAnnotations()
+	if annotations == nil {
+		annotations = make(map[string]string)
+		obj.SetAnnotations(annotations)
+	}
+	annotations[DeleteTagAnnotation] = "true"
+}
+
+func IsObjectTaggedTorDelete(obj common.KubernetesObject) bool {
+	annotations := obj.GetAnnotations()
+	if annotations == nil {
+		return false
+	}
+
+	annotation, ok := annotations[DeleteTagAnnotation]
+	return ok && annotation == "true"
 }
