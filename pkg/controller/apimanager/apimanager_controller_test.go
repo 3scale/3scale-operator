0cf019c7667fac08c16236e18ca77861f41a77b7
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-04-01 16:59:59 +0200

    update go.mod

--- pkg/controller/apimanager/apimanager_controller_test.go
+++ pkg/controller/apimanager/apimanager_controller_test.go
@@ -2,13 +2,10 @@ package apimanager
 
 import (
 	"context"
-	"fmt"
 	"testing"
 
 	"github.com/3scale/3scale-operator/pkg/3scale/amp/operator"
-	"github.com/3scale/3scale-operator/pkg/3scale/amp/product"
 	appsv1alpha1 "github.com/3scale/3scale-operator/pkg/apis/apps/v1alpha1"
-	"github.com/3scale/3scale-operator/version"
 	monitoringv1 "github.com/coreos/prometheus-operator/pkg/apis/monitoring/v1"
 	grafanav1alpha1 "github.com/integr8ly/grafana-operator/v3/pkg/apis/integreatly/v1alpha1"
 	appsv1 "github.com/openshift/api/apps/v1"
