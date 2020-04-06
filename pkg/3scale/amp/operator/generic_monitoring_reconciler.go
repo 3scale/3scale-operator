1758e523a876c9ab226058df33038838f1a3c256
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-04-06 13:11:28 +0200

    monitoring: reconcile kubernetes-resources dashboards

---
+++ pkg/3scale/amp/operator/generic_monitoring_reconciler.go
@@ -0,0 +1,33 @@
+package operator
+
+import (
+	"github.com/3scale/3scale-operator/pkg/3scale/amp/component"
+	"sigs.k8s.io/controller-runtime/pkg/reconcile"
+)
+
+type GenericMonitoringReconciler struct {
+	BaseAPIManagerLogicReconciler
+}
+
+// blank assignment to verify that BaseReconciler implements reconcile.Reconciler
+var _ LogicReconciler = &GenericMonitoringReconciler{}
+
+func NewGenericMonitoringReconciler(baseAPIManagerLogicReconciler BaseAPIManagerLogicReconciler) *GenericMonitoringReconciler {
+	return &GenericMonitoringReconciler{
+		BaseAPIManagerLogicReconciler: baseAPIManagerLogicReconciler,
+	}
+}
+
+func (r *GenericMonitoringReconciler) Reconcile() (reconcile.Result, error) {
+	err := r.reconcileGrafanaDashboard(component.KubernetesResourcesByNamespaceGrafanaDashboard(r.apiManager.Namespace))
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	err = r.reconcileGrafanaDashboard(component.KubernetesResourcesByPodGrafanaDashboard(r.apiManager.Namespace))
+	if err != nil {
+		return reconcile.Result{}, err
+	}
+
+	return reconcile.Result{}, nil
+}
