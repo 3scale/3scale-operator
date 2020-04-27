64a38d0e098bb9b38d9b9515ec2c6de1bf81c188
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-04-27 17:27:54 +0200

    capabilities v1beta1 APIs and CRDs scaffolding

---
+++ pkg/apis/capabilities/v1beta1/doc.go
@@ -0,0 +1,4 @@
+// Package v1beta1 contains API Schema definitions for the capabilities v1beta1 API group
+// +k8s:deepcopy-gen=package,register
+// +groupName=capabilities.3scale.net
+package v1beta1
