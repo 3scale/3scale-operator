a215c057cfdaed448f577862e191b6d4c84f42ae
Miguel Soriano <msoriano@redhat.com>
2020-06-19 12:17:15 +0200

    Remove clarified comment about apimanager being required as backup options

--- pkg/backup/apimanager_backup_options.go
+++ pkg/backup/apimanager_backup_options.go
@@ -11,7 +11,7 @@ type APIManagerBackupOptions struct {
 	APIManagerBackupName       string                      `validate:"required"` // Name of the APIManagerBackup CR. NOT the APIManager cr name
 	APIManagerBackupUID        types.UID                   `validate:"required"` // UID of the APIManagerBackup CR
 	APIManagerName             string                      `validate:"required"` // Name of the APIManager CR. NOT the APIManagerBackup cr name
-	APIManager                 *appsv1alpha1.APIManager    `validate:"required"` // Should we make this required?
+	APIManager                 *appsv1alpha1.APIManager    `validate:"required"`
 	APIManagerBackupPVCOptions *APIManagerBackupPVCOptions `validate:"required"`
 	OCCLIImageURL              string                      `validate:"required"`
 }
