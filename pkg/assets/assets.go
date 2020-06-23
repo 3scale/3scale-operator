083752033e4f26b755952f0a7eb5a9f37e2f486d
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-06-23 15:09:00 +0200

    fix assets comment

--- pkg/assets/assets.go
+++ pkg/assets/assets.go
@@ -5,7 +5,7 @@ import (
 	"text/template"
 )
 
-// Important: Run "make generate" to regenerate code after modifying/adding/removing any asset
+// Important: Run "make assets" to regenerate code after modifying/adding/removing any asset
 //go:generate go-bindata --prefix assets -pkg $GOPACKAGE -o bindata.go assets/...
 
 // SafeStringAsset Returns asset data as string
@@ -19,7 +19,7 @@ func SafeStringAsset(name string) string {
 	return string(data)
 }
 
-// SafeStringAsset Returns asset data as string
+// TemplateAsset Executes one tamplate by applying it to a daata structure.
 // panic if not found or any err is detected
 func TemplateAsset(name string, data interface{}) string {
 	tObj, err := template.New(name).Parse(SafeStringAsset(name))
