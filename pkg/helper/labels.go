70283af743ba1e8dfda7bfc6a3e2ba14a5d2de0d
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-06-30 15:34:08 +0200

    metering labels validate label max length

--- pkg/helper/labels.go
+++ pkg/helper/labels.go
@@ -2,6 +2,8 @@ package helper
 
 import (
 	"github.com/3scale/3scale-operator/pkg/3scale/amp/product"
+
+	"k8s.io/apimachinery/pkg/util/validation"
 )
 
 type ComponentType string
@@ -12,11 +14,16 @@ const (
 )
 
 func MeteringLabels(componentName, componentVersion string, componentType ComponentType) map[string]string {
-	return map[string]string{
-		"com.redhat.product-name":      "3scale",
-		"com.redhat.component-name":    componentName,
-		"com.redhat.product-version":   product.ThreescaleRelease,
-		"com.redhat.component-version": componentVersion,
-		"com.redhat.component-type":    string(componentType),
+	labels := map[string]string{
+		"com.redhat.product-name":    "3scale",
+		"com.redhat.component-name":  componentName,
+		"com.redhat.product-version": product.ThreescaleRelease,
+		"com.redhat.component-type":  string(componentType),
 	}
+
+	if len(componentVersion) <= validation.LabelValueMaxLength {
+		labels["com.redhat.component-version"] = componentVersion
+	}
+
+	return labels
 }
