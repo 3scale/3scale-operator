92cae20fa6ae36191e941ae8691d29b3a15e9e77
Eguzki Astiz Lezaun <eastizle@redhat.com>
2020-07-27 15:21:24 +0200

    backend controller: remove unused method

--- pkg/controller/backend/backend_controller.go
+++ pkg/controller/backend/backend_controller.go
@@ -201,37 +201,6 @@ func (r *ReconcileBackend) reconcile(backendResource *capabilitiesv1beta1.Backen
 	return reconcile.Result{}, nil
 }
 
-func (r *ReconcileBackend) reconcileSpec(backendResource *capabilitiesv1beta1.Backend) (*helper.BackendAPIEntity, error) {
-	err := r.validateSpec(backendResource)
-	if err != nil {
-		// Do not wrap error
-		return nil, err
-	}
-
-	providerAccount, err := helper.LookupProviderAccount(r.Client(), backendResource.Namespace, backendResource.Spec.ProviderAccountRef, r.Logger())
-	if err != nil {
-		return nil, fmt.Errorf("reconcile backend spec: %w", err)
-	}
-
-	threescaleAPIClient, err := helper.PortaClient(providerAccount)
-	if err != nil {
-		return nil, fmt.Errorf("reconcile backend spec: %w", err)
-	}
-
-	backendRemoteIndex, err := helper.NewBackendAPIRemoteIndex(threescaleAPIClient, r.Logger())
-	if err != nil {
-		return nil, fmt.Errorf("reconcile backend spec: %w", err)
-	}
-
-	reconciler := NewThreescaleReconciler(r.BaseReconciler, backendResource, threescaleAPIClient, backendRemoteIndex)
-	backendAPIEntity, err := reconciler.Reconcile()
-	if err != nil {
-		return nil, fmt.Errorf("Error 3scale sync: %w", err)
-	}
-
-	return backendAPIEntity, nil
-}
-
 func (r *ReconcileBackend) validateSpec(backendResource *capabilitiesv1beta1.Backend) error {
 	errors := field.ErrorList{}
 	// internal validation
