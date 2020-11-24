ddf8a126af272be1a0bd89032ec96a203a37056b
Miguel Soriano <msoriano@redhat.com>
2020-11-24 12:09:31 +0100

    Update CRDs validation tests to use versioned OpenAPI at CRD level

--- test/crds/crd_validation_test.go
+++ test/crds/crd_validation_test.go
@@ -29,26 +29,53 @@ const (
 	productPoliciesConfigurationPath         = "/spec/policies/configuration"
 )
 
+type testCRInfo struct {
+	crPrefix   string
+	apiVersion string
+}
+
 func TestSampleCustomResources(t *testing.T) {
 	schemaRoot := "../../bundle/manifests"
 	samplesRoot := "../../config/samples"
 	// Map of CRD:CR_sample_prefix
-	crdCrMap := map[string]string{
-		"apps.3scale.net_apimanagers.yaml":        "apps_v1alpha1_apimanager_",
-		"apps.3scale.net_apimanagerbackups.yaml":  "apps_v1alpha1_apimanagerbackup.yaml",
-		"apps.3scale.net_apimanagerrestores.yaml": "apps_v1alpha1_apimanagerrestore.yaml",
-		"capabilities.3scale.net_tenants.yaml":    "capabilities_v1alpha1_tenant",
-		"capabilities.3scale.net_backends.yaml":   "capabilities_v1beta1_backend",
-		"capabilities.3scale.net_products.yaml":   "capabilities_v1beta1_product",
-		"capabilities.3scale.net_openapis.yaml":   "capabilities_v1beta1_openapi",
+	crdCrMap := map[string]testCRInfo{
+		"apps.3scale.net_apimanagers.yaml": testCRInfo{
+			crPrefix:   "apps_v1alpha1_apimanager_",
+			apiVersion: apps.GroupVersion.Version,
+		},
+		"apps.3scale.net_apimanagerbackups.yaml": testCRInfo{
+			crPrefix:   "apps_v1alpha1_apimanagerbackup.yaml",
+			apiVersion: apps.GroupVersion.Version,
+		},
+		"apps.3scale.net_apimanagerrestores.yaml": testCRInfo{
+			crPrefix:   "apps_v1alpha1_apimanagerrestore.yaml",
+			apiVersion: apps.GroupVersion.Version,
+		},
+		"capabilities.3scale.net_tenants.yaml": testCRInfo{
+			crPrefix:   "capabilities_v1alpha1_tenant",
+			apiVersion: capabilitiesv1alpha1.GroupVersion.Version,
+		},
+		"capabilities.3scale.net_backends.yaml": testCRInfo{
+			crPrefix:   "capabilities_v1beta1_backend",
+			apiVersion: capabilitiesv1beta1.GroupVersion.Version,
+		},
+		"capabilities.3scale.net_products.yaml": testCRInfo{
+			crPrefix:   "capabilities_v1beta1_product",
+			apiVersion: capabilitiesv1beta1.GroupVersion.Version,
+		},
+		"capabilities.3scale.net_openapis.yaml": testCRInfo{
+			crPrefix:   "capabilities_v1beta1_openapi",
+			apiVersion: capabilitiesv1beta1.GroupVersion.Version,
+		},
 	}
-	for crd, prefix := range crdCrMap {
-		validateCustomResources(t, schemaRoot, samplesRoot, crd, prefix)
+
+	for crd, elem := range crdCrMap {
+		validateCustomResources(t, schemaRoot, samplesRoot, crd, elem.crPrefix, elem.apiVersion)
 	}
 }
 
-func validateCustomResources(t *testing.T, schemaRoot, samplesRoot, crd, prefix string) {
-	schema := getSchema(t, fmt.Sprintf("%s/%s", schemaRoot, crd))
+func validateCustomResources(t *testing.T, schemaRoot, samplesRoot, crd, prefix string, version string) {
+	schema := getSchemaVersioned(t, fmt.Sprintf("%s/%s", schemaRoot, crd), version)
 	assert.NotNil(t, schema)
 	walkFunc := func(path string, info os.FileInfo, err error) error {
 		if strings.HasPrefix(info.Name(), prefix) {
@@ -66,16 +93,42 @@ func validateCustomResources(t *testing.T, schemaRoot, samplesRoot, crd, prefix 
 	assert.NoError(t, err, "Error reading CR yaml files from ", samplesRoot)
 }
 
+type testCRDInfo struct {
+	obj        interface{}
+	apiVersion string
+}
+
 func TestCompleteCRD(t *testing.T) {
 	root := "../../bundle/manifests"
-	crdStructMap := map[string]interface{}{
-		"apps.3scale.net_apimanagers.yaml":        &apps.APIManager{},
-		"apps.3scale.net_apimanagerbackups.yaml":  &apps.APIManagerBackup{},
-		"apps.3scale.net_apimanagerrestores.yaml": &apps.APIManagerRestore{},
-		"capabilities.3scale.net_tenants.yaml":    &capabilitiesv1alpha1.Tenant{},
-		"capabilities.3scale.net_backends.yaml":   &capabilitiesv1beta1.Backend{},
-		"capabilities.3scale.net_products.yaml":   &capabilitiesv1beta1.Product{},
-		"capabilities.3scale.net_openapis.yaml":   &capabilitiesv1beta1.OpenAPI{},
+	crdStructMap := map[string]testCRDInfo{
+		"apps.3scale.net_apimanagers.yaml": testCRDInfo{
+			obj:        &apps.APIManager{},
+			apiVersion: apps.GroupVersion.Version,
+		},
+		"apps.3scale.net_apimanagerbackups.yaml": testCRDInfo{
+			obj:        &apps.APIManagerBackup{},
+			apiVersion: apps.GroupVersion.Version,
+		},
+		"apps.3scale.net_apimanagerrestores.yaml": testCRDInfo{
+			obj:        &apps.APIManagerRestore{},
+			apiVersion: apps.GroupVersion.Version,
+		},
+		"capabilities.3scale.net_tenants.yaml": testCRDInfo{
+			obj:        &capabilitiesv1alpha1.Tenant{},
+			apiVersion: capabilitiesv1alpha1.GroupVersion.Version,
+		},
+		"capabilities.3scale.net_backends.yaml": testCRDInfo{
+			obj:        &capabilitiesv1beta1.Backend{},
+			apiVersion: capabilitiesv1beta1.GroupVersion.Version,
+		},
+		"capabilities.3scale.net_products.yaml": testCRDInfo{
+			obj:        &capabilitiesv1beta1.Product{},
+			apiVersion: capabilitiesv1beta1.GroupVersion.Version,
+		},
+		"capabilities.3scale.net_openapis.yaml": testCRDInfo{
+			obj:        &capabilitiesv1beta1.OpenAPI{},
+			apiVersion: capabilitiesv1beta1.GroupVersion.Version,
+		},
 	}
 
 	pathOmissions := []string{
@@ -89,10 +142,10 @@ func TestCompleteCRD(t *testing.T) {
 		productPoliciesConfigurationPath,
 	}
 
-	for crd, obj := range crdStructMap {
+	for crd, elem := range crdStructMap {
 		t.Run(crd, func(subT *testing.T) {
-			schema := getSchema(subT, fmt.Sprintf("%s/%s", root, crd))
-			missingEntries := schema.GetMissingEntries(obj)
+			schema := getSchemaVersioned(subT, fmt.Sprintf("%s/%s", root, crd), elem.apiVersion)
+			missingEntries := schema.GetMissingEntries(elem.obj)
 			for _, missing := range missingEntries {
 
 				if missingFieldPathInPathOmissions(missing.Path, pathOmissions) {
@@ -108,6 +161,14 @@ func getSchema(t *testing.T, crd string) validation.Schema {
 	bytes, err := ioutil.ReadFile(crd)
 	assert.NoError(t, err, "Error reading CRD yaml from %v", crd)
 	schema, err := validation.New(bytes)
+	assert.NoError(t, err)
+	return schema
+}
+
+func getSchemaVersioned(t *testing.T, crd string, version string) validation.Schema {
+	bytes, err := ioutil.ReadFile(crd)
+	assert.NoError(t, err, "Error reading CRD yaml from %v", crd)
+	schema, err := validation.NewVersioned(bytes, version)
 	assert.NoError(t, err)
 	return schema
 }
