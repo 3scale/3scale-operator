6414bf93fc109a20ad90eea06c0eacb9f662946a
Eguzki Astiz Lezaun <eastizle@redhat.com>
2021-03-02 18:24:17 +0100

    application_plan_entity unittests

--- pkg/controller/helper/helper_test.go
+++ pkg/controller/helper/helper_test.go
@@ -1,11 +1,53 @@
 package helper
 
 import (
+	"fmt"
+	"net/http"
+	"path/filepath"
+	"reflect"
+	"runtime"
+	"testing"
+
 	"github.com/3scale/3scale-operator/pkg/helper"
 
+	threescaleapi "github.com/3scale/3scale-porta-go-client/client"
 	v1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
+
+// assert fails the test if the condition is false.
+func assert(tb testing.TB, condition bool, msg string, v ...interface{}) {
+	if !condition {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: "+msg+"\033[39m\n\n", append([]interface{}{filepath.Base(file), line}, v...)...)
+		tb.FailNow()
+	}
+}
+
+// ok fails the test if an err is not nil.
+func ok(tb testing.TB, err error) {
+	if err != nil {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d: unexpected error: %s\033[39m\n\n", filepath.Base(file), line, err.Error())
+		tb.FailNow()
+	}
+}
+
+// equals fails the test if exp is not equal to act.
+func equals(tb testing.TB, exp, act interface{}) {
+	if !reflect.DeepEqual(exp, act) {
+		_, file, line, _ := runtime.Caller(1)
+		fmt.Printf("\033[31m%s:%d:\n\n\texp: %#v\n\n\tgot: %#v\033[39m\n\n", filepath.Base(file), line, exp, act)
+		tb.FailNow()
+	}
+}
+
+func NewTestAdminPortal(t *testing.T) *threescaleapi.AdminPortal {
+	t.Helper()
+	ap, err := threescaleapi.NewAdminPortalFromStr("https://www.test.com:443")
+	ok(t, err)
+	return ap
+}
 
 func GetTestSecret(namespace, secretName string, data map[string]string) *v1.Secret {
 	secret := &v1.Secret{
@@ -19,4 +61,19 @@ func GetTestSecret(namespace, secretName string, data map[string]string) *v1.Sec
 	}
 	secret.Data = helper.GetSecretDataFromStringData(secret.StringData)
 	return secret
+}
+
+// RoundTripFunc .
+type RoundTripFunc func(req *http.Request) *http.Response
+
+// RoundTrip .
+func (f RoundTripFunc) RoundTrip(req *http.Request) (*http.Response, error) {
+	return f(req), nil
+}
+
+//NewTestClient returns *http.Client with Transport replaced to avoid making real calls
+func NewTestClient(fn RoundTripFunc) *http.Client {
+	return &http.Client{
+		Transport: RoundTripFunc(fn),
+	}
 }
