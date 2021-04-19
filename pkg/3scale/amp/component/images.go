acf6b1d59ec586a293ca9f0e8cd14f786d9ea544
Miguel Soriano <msoriano@redhat.com>
2021-04-19 13:02:55 +0200

    prepare 2.10-stable: update component default image urls

--- pkg/3scale/amp/component/images.go
+++ pkg/3scale/amp/component/images.go
@@ -1,19 +1,19 @@
 package component
 
 func ApicastImageURL() string {
-	return "quay.io/3scale/apicast:nightly"
+	return "quay.io/3scale/3scale210:apicast-3scale-2.10.0-GA"
 }
 
 func BackendImageURL() string {
-	return "quay.io/3scale/apisonator:nightly"
+	return "quay.io/3scale/3scale210:apisonator-3scale-2.10.0-GA"
 }
 
 func SystemImageURL() string {
-	return "quay.io/3scale/porta:nightly"
+	return "quay.io/3scale/3scale210:porta-3scale-2.10.0-GA"
 }
 
 func ZyncImageURL() string {
-	return "quay.io/3scale/zync:nightly"
+	return "quay.io/3scale/3scale10:zync-3scale-2.10.0-GA"
 }
 
 func BackendRedisImageURL() string {
