b52fc810e995a9de174385b45eac72a0294677dd
Eguzki Astiz Lezaun <eastizle@redhat.com>
2021-09-14 12:52:10 +0200

    enhance docker image version parser

--- pkg/helper/version_parser.go
+++ pkg/helper/version_parser.go
@@ -22,10 +22,14 @@ var (
 	reg1digitRawParser versionParser = func(text string) string { return regexp.MustCompile(`\d+`).FindString(text) }
 )
 
-func normalizeDockerImage(image string) string {
+func normalizeDockerURL(image string) string {
+	// Only for version parsing purposes, Host and scheme is added to have the URL being parsed correctly
+	// normalizeDockerURL tries to build a URL string that can be parsed by net/url library
+	// docker image URLS can be tricky sometimes.
+	// o := url.Parse("registry.redhat.io:443/rhel8/redis-5:1") -> o.Path is empty
 	newImage := image
 
-	if strings.Index(image, "/") < 0 {
+	if !strings.Contains(image, "/") {
 		newImage = fmt.Sprintf("docker.io/%s", newImage)
 	}
 
@@ -33,21 +37,22 @@ func normalizeDockerImage(image string) string {
 		newImage = fmt.Sprintf("http://%s", newImage)
 	}
 
-	return newImage
+	u, err := url.Parse(newImage)
+	if err != nil {
+		// silently discard the error and return empty
+		return ""
+	}
+
+	return u.Path
 }
 
 func ParseVersion(image string) string {
 	regExpsFuncs := []versionParser{reg3digitRawParser, reg2digitRawParser, regColonRawParser, reg1digitRawParser}
 
-	nImage := normalizeDockerImage(image)
-
-	u, err := url.Parse(nImage)
-	if err != nil {
-		return "unknown"
-	}
+	nImage := normalizeDockerURL(image)
 
 	for _, regExpRawFunc := range regExpsFuncs {
-		version := regExpRawFunc(u.Path)
+		version := regExpRawFunc(nImage)
 		if len(version) > 0 {
 			return version
 		}
