35a4cd40eddec2f589fbef4442168f54975120a7
Austin Cunningham <aucunnin@redhat.com>
2021-11-12 10:32:50 +0000

    removed tabs added spaces to main.go

--- main.go
+++ main.go
@@ -97,7 +97,7 @@ func main() {
 	}
 
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
-		Namespace: 			namespace,
+		Namespace:          namespace,
 		Scheme:             scheme,
 		MetricsBindAddress: metricsAddr,
 		Port:               9443,
