fad0fdf36b6a189e3832ce5da7e5022c0436ca0b
Austin Cunningham <aucunnin@redhat.com>
2022-01-31 11:27:00 +0000

    remove comment from tenants controller

--- controllers/capabilities/tenant_controller.go
+++ controllers/capabilities/tenant_controller.go
@@ -123,7 +123,6 @@ func (r *TenantReconciler) FetchMasterCredentials(k8sClient client.Client, tenan
 	err := k8sClient.Get(context.TODO(),
 		types.NamespacedName{
 			Name: tenantR.Spec.MasterCredentialsRef.Name,
-			// Master credential secret MUST be on same namespace as tenant CR
 			Namespace: tenantR.Spec.MasterCredentialsRef.Namespace,
 		},
 		masterCredentialsSecret)
