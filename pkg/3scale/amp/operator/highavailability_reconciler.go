f31d55f3ee4973c736bf65753596a6567cbbb8b9
Sergio Franco <sfrancog@redhat.com>
2022-04-11 11:22:38 +0100

    Remove unused HighAvailabilityReconciler
    In favour of new ExternalRependencyReconciler

--- pkg/3scale/amp/operator/highavailability_reconciler.go
+++ pkg/3scale/amp/operator/highavailability_reconciler.go
@@ -10,43 +10,6 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/reconcile"
 )
 
-type HighAvailabilityReconciler struct {
-	*BaseAPIManagerLogicReconciler
-}
-
-func NewHighAvailabilityReconciler(baseAPIManagerLogicReconciler *BaseAPIManagerLogicReconciler) *HighAvailabilityReconciler {
-	return &HighAvailabilityReconciler{
-		BaseAPIManagerLogicReconciler: baseAPIManagerLogicReconciler,
-	}
-}
-
-func (r *HighAvailabilityReconciler) Reconcile() (reconcile.Result, error) {
-	ha, err := HighAvailability(r.apiManager, r.Client())
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// Backend Redis Secret
-	err = r.ReconcileSecret(ha.BackendRedisSecret(), reconcilers.DefaultsOnlySecretMutator)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// System Redis Secret
-	err = r.ReconcileSecret(ha.SystemRedisSecret(), reconcilers.DefaultsOnlySecretMutator)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-
-	// System database Secret
-	err = r.ReconcileSecret(ha.SystemDatabaseSecret(), reconcilers.DefaultsOnlySecretMutator)
-	if err != nil {
-		return reconcile.Result{}, err
-	}
-
-	return reconcile.Result{}, nil
-}
-
 func HighAvailability(apimanager *appsv1alpha1.APIManager, client client.Client) (*component.HighAvailability, error) {
 	optsProvider := NewHighAvailabilityOptionsProvider(apimanager, apimanager.Namespace, client)
 	opts, err := optsProvider.GetHighAvailabilityOptions()
