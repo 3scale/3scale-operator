1cace96275bf682bd8931a812cd3e773777577af
Eguzki Astiz Lezaun <eastizle@redhat.com>
2022-06-13 17:45:58 +0200

    apimanagerrestore: fix apimanager available test

--- controllers/apps/apimanagerrestore_logic_reconciler.go
+++ controllers/apps/apimanagerrestore_logic_reconciler.go
@@ -2,8 +2,6 @@ package controllers
 
 import (
 	"fmt"
-	"reflect"
-	"sort"
 	"time"
 
 	appsv1alpha1 "github.com/3scale/3scale-operator/apis/apps/v1alpha1"
@@ -156,12 +154,12 @@ func (r *APIManagerRestoreLogicReconciler) reconcileRestoreFromPVCSource() (reco
 		return res, err
 	}
 
-	res, err = r.reconcileWaitForAPIManagerReady()
+	res, err = r.reconcileResynchronizeZyncDomains()
 	if res.Requeue || err != nil {
 		return res, err
 	}
 
-	res, err = r.reconcileResynchronizeZyncDomains()
+	res, err = r.reconcileWaitForAPIManagerReady()
 	if res.Requeue || err != nil {
 		return res, err
 	}
@@ -458,28 +456,8 @@ func (r *APIManagerRestoreLogicReconciler) reconcileWaitForAPIManagerReady() (re
 		return reconcile.Result{}, err
 	}
 
-	// External databases scenario assumed
-	expectedDeploymentNames := []string{
-		"apicast-production",
-		"apicast-staging",
-		"backend-listener",
-		"backend-worker",
-		"backend-cron",
-		"zync",
-		"zync-que",
-		"zync-database",
-		"system-app",
-		"system-sphinx",
-		"system-sidekiq",
-		"system-memcache",
-	}
-
-	existingReadyDeployments := existingAPIManager.Status.Deployments.Ready
-	sort.Slice(expectedDeploymentNames, func(i, j int) bool { return expectedDeploymentNames[i] < expectedDeploymentNames[j] })
-	sort.Slice(existingReadyDeployments, func(i, j int) bool { return existingReadyDeployments[i] < existingReadyDeployments[j] })
-
-	if !reflect.DeepEqual(existingReadyDeployments, expectedDeploymentNames) {
-		r.Logger().Info("all APIManager Deployments not ready. Waiting", "APIManager", existingAPIManager.Name, "expected-ready-deployments", expectedDeploymentNames, "ready-deployments", existingReadyDeployments)
+	if existingAPIManager.Status.Conditions.IsFalseFor(appsv1alpha1.APIManagerAvailableConditionType) {
+		r.Logger().Info("APIManager not ready. Waiting", "APIManager", existingAPIManager.Name)
 		return reconcile.Result{RequeueAfter: 5 * time.Second, Requeue: true}, nil
 	}
 
