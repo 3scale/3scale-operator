893262b4a880d34525ecde126e3d50bb871d1d1d
MStokluska <mstoklus@redhat.com>
2023-12-11 14:31:21 +0000

    THREESCALE-10090 address comments

--- controllers/capabilities/tenant_status_reconciler.go
+++ controllers/capabilities/tenant_status_reconciler.go
@@ -11,19 +11,17 @@ import (
 
 type TenantStatusReconciler struct {
 	*reconcilers.BaseReconciler
-	resourceUpdated  *capabilitiesv1alpha1.Tenant
-	resourceOriginal *capabilitiesv1alpha1.Tenant
-	reconcileError   error
-	logger           logr.Logger
+	tenantResource *capabilitiesv1alpha1.Tenant
+	reconcileError error
+	logger         logr.Logger
 }
 
-func NewTenantStatusReconciler(b *reconcilers.BaseReconciler, resourceUpdated, resourceOriginal *capabilitiesv1alpha1.Tenant, reconcileError error) *TenantStatusReconciler {
+func NewTenantStatusReconciler(b *reconcilers.BaseReconciler, tenantResource *capabilitiesv1alpha1.Tenant, reconcileError error) *TenantStatusReconciler {
 	return &TenantStatusReconciler{
-		BaseReconciler:   b,
-		resourceUpdated:  resourceUpdated,
-		resourceOriginal: resourceOriginal,
-		reconcileError:   reconcileError,
-		logger:           b.Logger().WithValues("Status Reconciler", resourceUpdated.Name),
+		BaseReconciler: b,
+		tenantResource: tenantResource,
+		reconcileError: reconcileError,
+		logger:         b.Logger().WithValues("Status Reconciler", tenantResource.Name),
 	}
 }
 
@@ -33,12 +31,12 @@ func (s *TenantStatusReconciler) Reconcile() (bool, error) {
 	equalStatus := true
 	// Check for changes to the status
 	newStatus := s.calculateStatus()
-	equalStatus = s.resourceOriginal.Status.StatusEqual(&newStatus, s.logger)
+	equalStatus = s.tenantResource.Status.StatusEqual(&newStatus, s.logger)
 
 	if !equalStatus {
 		s.logger.Info("updating tenant status")
-		s.resourceUpdated.Status = newStatus
-		err := s.UpdateResourceStatus(s.resourceUpdated)
+		s.tenantResource.Status = newStatus
+		err := s.UpdateResourceStatus(s.tenantResource)
 		if err != nil {
 			s.logger.Info("ERROR", "error in tenant status reconciler when updating the resource", err)
 			return equalStatus, err
@@ -49,8 +47,8 @@ func (s *TenantStatusReconciler) Reconcile() (bool, error) {
 }
 
 func (s *TenantStatusReconciler) calculateStatus() capabilitiesv1alpha1.TenantStatus {
-	status := s.resourceUpdated.Status
-	status.Conditions = s.resourceUpdated.Status.Conditions.Copy()
+	status := s.tenantResource.Status
+	status.Conditions = s.tenantResource.Status.Conditions.Copy()
 	status.Conditions.SetCondition(s.readyCondition())
 
 	return status
@@ -62,7 +60,7 @@ func (s *TenantStatusReconciler) readyCondition() common.Condition {
 		Status: corev1.ConditionFalse,
 	}
 
-	if s.reconcileError == nil && s.resourceUpdated.Status.TenantId != 0 && s.resourceUpdated.Status.AdminId != 0 {
+	if s.reconcileError == nil && s.tenantResource.Status.TenantId != 0 && s.tenantResource.Status.AdminId != 0 {
 		condition.Status = corev1.ConditionTrue
 	}
 
