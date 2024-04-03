d7e5757f2daa8f6edede553f0f1fe36dc98ebb56
Carl Kyrillos <ckyrillo@redhat.com>
2024-04-03 14:19:35 -0400

    THREESCALE-10949 Preserve DC annotations, labels, and env vars

--- pkg/upgrade/migrate_deployment_config_2.15.go
+++ pkg/upgrade/migrate_deployment_config_2.15.go
@@ -7,6 +7,7 @@ import (
 	appsv1 "github.com/openshift/api/apps/v1"
 	routev1 "github.com/openshift/api/route/v1"
 	k8sappsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
 	k8serr "k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
@@ -54,6 +55,24 @@ func MigrateDeploymentConfigToDeployment(dName string, dNamespace string, overri
 		return false, nil
 	}
 
+	// Copy any custom labels from the DeploymentConfig to the Deployment
+	err = transferDeploymentConfigLabels(deploymentConfig, deployment, client)
+	if err != nil {
+		return false, fmt.Errorf("error transferring deploymentconfig labels to deployment %s: %v", deployment.Name, err)
+	}
+
+	// Copy any custom annotations from the DeploymentConfig to the Deployment
+	err = transferDeploymentConfigAnnotations(deploymentConfig, deployment, client)
+	if err != nil {
+		return false, fmt.Errorf("error transferring deploymentconfig annotations to deployment %s: %v", deployment.Name, err)
+	}
+
+	// Copy any custom container env vars from the DeploymentConfig to the Deployment
+	err = transferDeploymentConfigEnvVars(deploymentConfig, deployment, client)
+	if err != nil {
+		return false, fmt.Errorf("error transferring deploymentconfig env vars to deployment %s: %v", deployment.Name, err)
+	}
+
 	// Requeue if Deployment isn't healthy and override is set to false, otherwise proceed
 	if !helper.IsDeploymentAvailable(deployment) && !overrideDeploymentHealth {
 		log.V(1).Info(fmt.Sprintf("deployment %s is not yet available and overrideDeploymentHealth is set to %t", deployment.Name, overrideDeploymentHealth))
@@ -78,6 +97,172 @@ func MigrateDeploymentConfigToDeployment(dName string, dNamespace string, overri
 
 	log.Info(fmt.Sprintf("%s Deployment has replaced its corresponding DeploymentConfig", deployment.Name))
 	return true, nil
+}
+
+func transferDeploymentConfigLabels(dc *appsv1.DeploymentConfig, deployment *k8sappsv1.Deployment, client k8sclient.Client) error {
+	// Get the DeploymentConfig-level labels from the DeploymentConfig and Deployment
+	dcLabels := dc.ObjectMeta.Labels
+	dLabels := deployment.ObjectMeta.Labels
+
+	// Determine which DeploymentConfig-level labels exist in dc but not in deployment
+	missingLabels := make(map[string]string)
+	for key, value := range dcLabels {
+		if _, exists := dLabels[key]; !exists {
+			missingLabels[key] = value
+		}
+	}
+
+	// Add the missing DeploymentConfig-level labels to deployment
+	if len(missingLabels) > 0 {
+		for key, value := range missingLabels {
+			if deployment.ObjectMeta.Labels == nil {
+				deployment.ObjectMeta.Labels = map[string]string{}
+			}
+			deployment.ObjectMeta.Labels[key] = value
+		}
+	}
+
+	// Extract the Pod-level labels from the DeploymentConfig and Deployment
+	dcPodLabels := dc.Spec.Template.Labels
+	dPodLabels := deployment.Spec.Template.Labels
+
+	// Determine which Pod-level labels exist in dc but not in deployment
+	missingPodLabels := make(map[string]string)
+	for key, value := range dcPodLabels {
+		if _, exists := dPodLabels[key]; !exists {
+			// Skip adding the old deploymentConfig label
+			if key != "deploymentConfig" {
+				missingPodLabels[key] = value
+			}
+		}
+	}
+
+	// Add any missing Pod-level labels to deployment
+	if len(missingPodLabels) > 0 {
+		for key, value := range missingPodLabels {
+			if deployment.Spec.Template.Labels == nil {
+				deployment.Spec.Template.Labels = map[string]string{}
+			}
+			deployment.Spec.Template.Labels[key] = value
+		}
+	}
+
+	if len(missingLabels) > 0 || len(missingPodLabels) > 0 {
+		// Update the deployment
+		err := client.Update(context.TODO(), deployment)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func transferDeploymentConfigAnnotations(dc *appsv1.DeploymentConfig, deployment *k8sappsv1.Deployment, client k8sclient.Client) error {
+	// Get the DeploymentConfig-level annotations from the DeploymentConfig and Deployment
+	dcAnnotations := dc.ObjectMeta.Annotations
+	dAnnotations := deployment.ObjectMeta.Annotations
+
+	// Determine which DeploymentConfig-level annotations exist in dc but not in deployment
+	missingAnnotations := make(map[string]string)
+	for key, value := range dcAnnotations {
+		if _, exists := dAnnotations[key]; !exists {
+			missingAnnotations[key] = value
+		}
+	}
+
+	// Add the missing DeploymentConfig-level annotations to deployment
+	if len(missingAnnotations) > 0 {
+		for key, value := range missingAnnotations {
+			if deployment.ObjectMeta.Annotations == nil {
+				deployment.ObjectMeta.Annotations = map[string]string{}
+			}
+			deployment.ObjectMeta.Annotations[key] = value
+		}
+	}
+
+	// Extract the Pod-level annotations from the DeploymentConfig and Deployment
+	dcPodAnnotations := dc.Spec.Template.Annotations
+	dPodAnnotations := deployment.Spec.Template.Annotations
+
+	// Determine which Pod-level annotations exist in dc but not in deployment
+	missingPodAnnotations := make(map[string]string)
+	for key, value := range dcPodAnnotations {
+		if _, exists := dPodAnnotations[key]; !exists {
+			missingPodAnnotations[key] = value
+		}
+	}
+
+	// Add any missing Pod-level annotations to deployment
+	if len(missingPodAnnotations) > 0 {
+		for key, value := range missingPodAnnotations {
+			if deployment.Spec.Template.ObjectMeta.Annotations == nil {
+				deployment.Spec.Template.ObjectMeta.Annotations = map[string]string{}
+			}
+			deployment.Spec.Template.Annotations[key] = value
+		}
+	}
+
+	if len(missingAnnotations) > 0 || len(missingPodAnnotations) > 0 {
+		// Update the deployment
+		err := client.Update(context.TODO(), deployment)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func transferDeploymentConfigEnvVars(dc *appsv1.DeploymentConfig, deployment *k8sappsv1.Deployment, client k8sclient.Client) error {
+	envVarsAdded := false
+
+	// Loop through each container in dc
+	for _, dcContainer := range dc.Spec.Template.Spec.Containers {
+		// Find corresponding container in deployment
+		var dContainer *corev1.Container
+		for i, container := range deployment.Spec.Template.Spec.Containers {
+			if dcContainer.Name == container.Name {
+				dContainer = &deployment.Spec.Template.Spec.Containers[i]
+				break
+			}
+		}
+
+		if dContainer == nil {
+			return fmt.Errorf("the deployment %s is missing the container %s", deployment.Name, dcContainer.Name)
+		}
+
+		// Determine which env vars exist in dc's container but not in deployment's container
+		var missingEnvVars []corev1.EnvVar
+		for _, dcEnvVar := range dcContainer.Env {
+			found := false
+			for _, deploymentEnvVar := range dContainer.Env {
+				if dcEnvVar.Name == deploymentEnvVar.Name {
+					found = true
+					break
+				}
+			}
+			if !found {
+				missingEnvVars = append(missingEnvVars, dcEnvVar)
+			}
+		}
+
+		// Add any missing env vars to deployment's container
+		if len(missingEnvVars) > 0 {
+			dContainer.Env = append(dContainer.Env, missingEnvVars...)
+			envVarsAdded = true
+		}
+	}
+
+	if envVarsAdded {
+		// Update the deployment
+		err := client.Update(context.TODO(), deployment)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
 }
 
 func transferZyncRoutesOwnership(deployment *k8sappsv1.Deployment, client k8sclient.Client, scheme *runtime.Scheme) error {
