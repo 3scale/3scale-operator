9271591ea2eed39d9776e1c0a478e5b5f5fda003
MStokluska <mstoklus@redhat.com>
2025-01-13 12:54:40 +0000

    THREESCALE-11508 update applications updates handling to remove nil pointer when status.ID isn't available

--- controllers/capabilities/applications.go
+++ controllers/capabilities/applications.go
@@ -24,31 +24,28 @@ func (t *ApplicationThreescaleReconciler) syncApplication(_ interface{}) error {
 	if t.applicationEntity.PlanID() != planID {
 		_, err := t.threescaleAPIClient.ChangeApplicationPlan(*t.accountResource.Status.ID, *t.applicationResource.Status.ID, planID)
 		if err != nil {
-			return fmt.Errorf("error sync applicaiton [%s;%d]: %w", t.applicationResource.Spec.Name, t.applicationEntity.ID(), err)
+			return fmt.Errorf("error sync application [%s;%d]: %w", t.applicationResource.Spec.Name, t.applicationEntity.ID(), err)
 		}
-		params["applicationPlanName"] = t.applicationResource.Spec.ApplicationPlanName
 	}
 
 	if t.applicationResource.Spec.Suspend == true && t.applicationEntity.ApplicationState() == "live" {
 		_, err := t.threescaleAPIClient.ApplicationSuspend(*t.accountResource.Status.ID, t.applicationEntity.ID())
 		if err != nil {
-			return fmt.Errorf("error sync applicaiton [%s;%d]: %w", t.applicationResource.Spec.Name, t.applicationEntity.ID(), err)
+			return fmt.Errorf("error sync application [%s;%d]: %w", t.applicationResource.Spec.Name, t.applicationEntity.ID(), err)
 		}
-		params["state"] = "suspended"
 	}
 
 	if t.applicationResource.Spec.Suspend == false && t.applicationEntity.ApplicationState() == "suspended" {
 		_, err := t.threescaleAPIClient.ApplicationResume(*t.accountResource.Status.ID, t.applicationEntity.ID())
 		if err != nil {
-			return fmt.Errorf("error sync applicaiton [%s;%d]: %w", t.applicationResource.Spec.Name, t.applicationEntity.ID(), err)
+			return fmt.Errorf("error sync application [%s;%d]: %w", t.applicationResource.Spec.Name, t.applicationEntity.ID(), err)
 		}
-		params["state"] = "live"
 	}
 
 	if len(params) > 0 {
 		_, err := t.threescaleAPIClient.UpdateApplication(*t.accountResource.Status.ID, *t.applicationResource.Status.ID, params)
 		if err != nil {
-			return fmt.Errorf("error sync applicaiton [%s;%d]: %w", t.applicationResource.Spec.Name, t.applicationEntity.ID(), err)
+			return fmt.Errorf("error sync application [%s;%d]: %w", t.applicationResource.Spec.Name, t.applicationEntity.ID(), err)
 		}
 	}
 
